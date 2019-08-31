18510cf6abb8f79b3954e9db5755a0db413ca9b1
siontama <siontama@naver.com>
2019-08-31 20:22:04 +0900

    middleware
    compression, body-parser

--- 생활코딩/web3 - Node.js - Express/main.js
+++ 생활코딩/web3 - Node.js - Express/main.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 var fs = require("fs");
 var path = require("path");
+var bodyParser = require("body-parser");
+var compression = require("compression");
 var template = require("./lib/template.js");
 var sanitizeHtml = require("sanitize-html");
 var qs = require("querystring");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
 
 // app.get("/", (req, res) => res.send("Heelo World!"));
 app.get("/", function(request, response) {
@@ -37,11 +42,11 @@ app.get("/page/:pageId", function(request, response) {
         list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
-            <a href="/update/${sanitizedTitle}">update</a>
-            <form action="/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
+          <a href="/update/${sanitizedTitle}">update</a>
+          <form action="/delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
       );
       response.send(html);
     });
@@ -56,7 +61,7 @@ app.get("/create", function(request, response) {
       title,
       list,
       `
-        <form action="/create" method="post">
+        <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -72,8 +77,8 @@ app.get("/create", function(request, response) {
   });
 });
 
-app.post("/create", function(request, response) {
-  var body = "";
+app.post("/create_process", function(request, response) {
+  /*var body = "";
   request.on("data", function(data) {
     body = body + data;
   });
@@ -85,6 +90,15 @@ app.post("/create", function(request, response) {
       response.writeHead(302, { Location: `/?id=${title}` });
       response.end();
     });
+  });
+  */
+
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, "utf8", function(err) {
+    response.writeHead(302, { Location: `/?id=${title}` });
+    response.end();
   });
 });
 
@@ -117,35 +131,23 @@ app.get("/update/:pageId", function(request, response) {
 });
 
 app.post("/update_process", function(request, response) {
-  var body = "";
-  request.on("data", function(data) {
-    body = body + data;
-  });
-  request.on("end", function() {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error) {
-      fs.writeFile(`data/${title}`, description, "utf8", function(err) {
-        response.redirect(`/?id=${title}`);
-      });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error) {
+    fs.writeFile(`data/${title}`, description, "utf8", function(err) {
+      response.redirect(`/?id=${title}`);
     });
   });
 });
 
 app.post("/delete_process", function(request, response) {
-  var body = "";
-  request.on("data", function(data) {
-    body = body + data;
-  });
-  request.on("end", function() {
-    var post = qs.parse(body);
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(error) {
-      response.redirect("/");
-    });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error) {
+    response.redirect("/");
   });
 });
 
