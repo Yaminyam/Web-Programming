1a615caa0caec9cffec9c78a4dbc4832030b5e97
siontama <siontama@naver.com>
2019-09-02 18:19:27 +0900

    path & domain

--- 생활코딩/web3 - Node.js - Cookie&Auth/nodejs/cookie.js
+++ 생활코딩/web3 - Node.js - Cookie&Auth/nodejs/cookie.js
@@ -12,7 +12,10 @@ http
         "yummy_cookie=choco",
         "tasty_cookie=strawberry",
         "Permanent=cookies; Max-Age=${60*60*24*30}",
-        "secure=Secure; Secure"
+        "Secure=Secure; Secure",
+        "HttpOnly=HttpOnly; HttpOnly",
+        "Path=Path; Path=/cookie",
+        "Domain=Domain; Domain=o2.org"
       ]
     });
     response.end("Cookie!!");
