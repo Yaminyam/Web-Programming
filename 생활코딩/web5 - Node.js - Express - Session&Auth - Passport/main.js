c5fc1884674325f6fc8a0e9573e458aa8fe9465b
siontama <siontama@naver.com>
2019-09-09 03:37:09 +0900

    passport.js 세션이용

--- 생활코딩/web5 - Node.js - Express - Session&Auth - Passport/main.js
+++ 생활코딩/web5 - Node.js - Express - Session&Auth - Passport/main.js
@@ -29,6 +29,19 @@ var authData = {
 var passport = require("passport"),
   LocalStrategy = require("passport-local").Strategy;
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  console.log("serializeUser", user);
+  done(null, user.email);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log("deserializeUser", id);
+  done(null, authData);
+});
+
 passport.use(
   new LocalStrategy(function(username, password, done) {
     if (username === authData.email) {
